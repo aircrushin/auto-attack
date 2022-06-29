626b4ba26e6da6a50647b51ad147e27bff295f57
aircrushin <1216564567@qq.com>
2022-06-29 16:51:11 +0800

    first

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import main from '../views/main.vue';
+
+const routes = [
+  {
+    path:'/',
+    name:'main',
+    component: main
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <el-calendar></el-calendar>
+  </div>
+</template>
